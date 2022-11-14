31426c32aa085654f0b68e065374fa79959d9b69
Jamie McIntyre <mcintyrejak@gmail.com>
2022-11-14 12:33:31 -0500

    Player switch on roll 1 functionality completed

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 "use strict";
 
-const rollDieBtn = document.querySelector(".roll-die-btn");
-const holdBtn = document.querySelector(".hold-btn");
+let currentPlayer = "p1";
+
 const newGameBtn = document.querySelector(".new-game-btn");
 
-let currentPlayer = "Player 2";
-
-// For rolls
+// FOR ROLLS
+const rollDieBtn = document.querySelector(".roll-die-btn");
 const die = document.querySelector(".die");
 const p1CurrentScoreEl = document.querySelector(".p1 .current-score");
 const p2CurrentScoreEl = document.querySelector(".p2 .current-score");
@@ -15,22 +14,38 @@ let roll = 0;
 let p1CurrentScore = 0;
 let p2CurrentScore = 0;
 
-if (currentPlayer === "Player 1") {
-  rollDieBtn.addEventListener("click", () => {
-    let dieNumber = getRandomNumber();
-    die.innerText = diceArray[dieNumber];
-    roll = dieNumber + 1;
+const rollDie = () => {
+  let dieNumber = getRandomNumber();
+  die.innerText = diceArray[dieNumber];
+  roll = dieNumber + 1;
+  if (roll === 1 && currentPlayer === "p1") {
+    currentPlayer = "p2";
+    p1CurrentScore = 0;
+  } else if (roll === 1 && currentPlayer === "p2") {
+    currentPlayer = "p1";
+    p2CurrentScore = 0;
+  }
+};
+
+rollDieBtn.addEventListener("click", () => {
+  rollDie();
+  if (currentPlayer === "p1" && roll !== 1) {
     p1CurrentScore += roll;
     p1CurrentScoreEl.innerText = p1CurrentScore;
-  });
-} else if (currentPlayer === "Player 2") {
-  rollDieBtn.addEventListener("click", () => {
-    let dieNumber = getRandomNumber();
-    die.innerText = diceArray[dieNumber];
-    roll = dieNumber + 1;
+  } else if (currentPlayer === "p2" && roll !== 1) {
     p2CurrentScore += roll;
     p2CurrentScoreEl.innerText = p2CurrentScore;
-  });
-}
+  }
+});
 
 const getRandomNumber = () => Math.floor(Math.random() * 6);
+
+//FOR HOLDS
+
+const holdBtn = document.querySelector(".hold-btn");
+const p1TotalScore = document.querySelector(".p1 .total-score");
+
+holdBtn.addEventListener("click", () => {
+  if (currentPlayer === "p1") {
+  }
+});
